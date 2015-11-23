603d524e9e8cf6ff1f2867ceed2ccd79ad45f7b5
Tom Booth <tombooth@gmail.com>
2015-11-23 02:01:46 +0000

    Add open! function to user ns, to make life better
    This function will open a web browser pointing at the url of the test
    webapp.
    
    This will only work on OS X.

--- README.md
+++ README.md
@@ -14,7 +14,9 @@ user=> (go)  ;; starts the example webapp on a random port
 ...
 ... c.beardandcode.components.web-server - Started web server on http://127.0.0.1:8080
 
+user=> (open!)  ;; only works on OSX
+
 ```
 
-Open the url found in your logs in the browser, it will likely be different to the one above.
+If you are not on OS X, open the url found in your logs it will likely be different to the one above.
 
--- dev/user.clj
+++ dev/user.clj
@@ -1,15 +1,20 @@
 (ns user
-  (:require [clojure.repl :refer :all]
+  (:require [clojure.java.shell :as shell]
+            [clojure.repl :refer :all]
             [clojure.test :refer [run-all-tests]]
             [clojure.tools.namespace.repl :refer [refresh refresh-all]]
             [reloaded.repl :refer [system init start stop go reset clear]]
             [vinyasa.reimport :refer [reimport]]
             [vinyasa.pull :refer [pull]]
+            [com.beardandcode.components.web-server :refer [port]]
             [com.beardandcode.users :refer :all]
             [com.beardandcode.users.test.webapp :refer [new-test-system]]))
 
 (reloaded.repl/set-init!
  #(new-test-system (Integer. (or (System/getenv "PORT") 0))))
+
+(defn url [] (str "http://localhost:" (-> system :web port) "/"))
+(defn open! [] (shell/sh "open" (url)))
 
 (defn refresh-and [f]
   (refresh :after (symbol "user" f)))
